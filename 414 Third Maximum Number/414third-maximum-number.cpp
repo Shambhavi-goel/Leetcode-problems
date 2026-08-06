2be91125c3b54e7e74f54ce80bb87d5e5c25af62
Shambhavi Goel <shambhavigoel2@gmail.com>
2026-08-06 17:40:29 +0000

    Code challenge submission for question: 414 Third Maximum Number

---
+++ 414 Third Maximum Number/414third-maximum-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int max= nums[nums.size()-1];
+        int count=1;
+        for(int i= (nums.size()-2); i>= 0; i--){
+            if(nums[i]<max){
+                max= nums[i];
+                count++;
+            } 
+            if(count==3){
+            return max;
+        }
+        }
+        return nums[nums.size()-1];
+    }
+};
